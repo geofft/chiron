4edcccf3e49618a13bc2608479b2c051ba115413
Geoffrey Thomas <geofft@mit.edu>
2015-04-10 14:11:48 -0400

    rusti fixes

--- rusti.py
+++ rusti.py
@@ -1,13 +1,11 @@
 import json
 import requests
 
+import chiron
+
 irc_template = """\
-#![feature(asm, simd, thread_local, unsafe_destructor)]
-#![feature(core, libc, collections, std_misc, io, path)]
 #![allow(dead_code, unused_variables)]
 #![allow(unused_features)]
-
-extern crate libc;
 
 fn show<T: std::fmt::Debug>(e: T) { println!("{:?}", e) }
 
@@ -20,18 +18,22 @@ fn main() {
 def evaluate(code):
     code = irc_template % {"input": code}
 
-    payload = json.dumps(dict(code=code, version="master", optimize="2"))
+    payload = json.dumps(dict(code=code, version="beta", optimize="2"))
     headers = {'Content-Type': 'application/json',
                'User-Agent': 'rusti for MIT Zephyr (geofft@ldpreload.com)'}
     response = requests.post('https://play.rust-lang.org/evaluate.json',
                              data=payload, headers=headers)
-    out = response.json()['result']
+    j = response.json()
+    if 'error' in j:
+        out = 'Error: ' + j['error']
+    else:
+        out = j['result']
 
     if len(out) > 5000:
-        return "more than 5000 bytes of output; bailing out"
+        return chiron.Response('rusti', "more than 5000 bytes of output; bailing out")
 
     out = out.replace(u"\xff", u"", 1)
     out = out.decode(errors="replace")
 
-    return 'rusti', out
+    return chiron.Response('rusti', out)
 
